8a4217f492fefaf83b333e77b63ea7981d83cd9d
Stefan Formentano <StefanFormentano@gmail.com>
2019-03-18 07:15:31 -0500

    build better security by disallowing blank register submissions and reworked API handling

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex({
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
-    password : '',
+    password : 'Peters13',
     database : 'facerecdb'
   }
 });
@@ -29,5 +29,6 @@ app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
 app.listen(3000);
