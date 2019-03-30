43e2f49da2fc82bf4ebfb89ed6f5595590fb463d
Stefan Formentano <StefanFormentano@gmail.com>
2019-03-29 21:11:26 -0500

    updating database location

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-tapered-86116',
     user : 'postgres',
     password : 'Peters13',
     database : 'facerecdb'
