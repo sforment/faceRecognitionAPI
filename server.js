6ea7c85caeec33e84f81a2b15603752e2376144c
Stefan Formentano <StefanFormentano@gmail.com>
2019-03-29 21:20:25 -0500

    bug fixing with server connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-tapered-86116',
-    user : 'postgres',
-    password : 'Peters13',
-    database : 'facerecdb'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
